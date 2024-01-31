61523a32f7de6b7b70ea2858c687d26a2c223a35
Anmol <ainsa2279@gmail.com>
2024-02-01 00:28:18 +0530

    Works on file not on buffer

--- index.py
+++ index.py
@@ -1,31 +1,40 @@
 from flask import Flask, request, jsonify
 from spafe.frequencies.dominant_frequencies import get_dominant_frequencies
+import librosa
 import numpy as np
 
 app = Flask(__name__)
 
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
 @app.route('/', methods=['POST'])
 def calculate_dominant_frequencies():
-    audio_file = request.files['audio']
- 
-    # Read the audio file using a suitable library (e.g., librosa, soundfile, wave)
-    fs, sig = read_audio_file(audio_file)  # Replace with your preferred audio reading function
+    print(request)
+    print(request.data)
+    try:
+        # audio_file = request.files['audio']
+        # sig, fs = librosa.load(audio_file, sr=None)
+        #
+        # dominant_frequencies = get_dominant_frequencies(sig, fs,
+        #                                                 butter_filter=False,
+        #                                                 lower_cutoff=0,
+        #                                                 upper_cutoff=fs/2,
+        #                                                 nfft=512,
+        #                                                 win_len=0.020,
+        #                                                 win_hop=0.010,
+        #                                                 win_type="hamming")
+        #
+        #
+        # print(dominant_frequencies)
+        #
+        # dominant_frequencies_list = list(dominant_frequencies)
 
-    # Compute dominant frequencies using spafe
-    dominant_frequencies = get_dominant_frequencies(sig,
-                                                    fs,
-                                                    butter_filter=False,
-                                                    lower_cutoff=0,
-                                                    upper_cutoff=fs/2,
-                                                    nfft=512,
-                                                    win_len=0.020,
-                                                    win_hop=0.010,
-                                                    win_type="hamming")
+        # return jsonify({'dominant_frequencies': dominant_frequencies_list})
+        return jsonify({ 'name':'flask' })
+    except Exception as error :
+        print(error)
+        return jsonify({ 'name':'flask' })
 
-    # Convert dominant frequencies to a list for JSON serialization
-    dominant_frequencies_list = list(dominant_frequencies)
-
-    return jsonify({'dominant_frequencies': dominant_frequencies_list})
 
 if __name__ == '__main__':
     app.run()
